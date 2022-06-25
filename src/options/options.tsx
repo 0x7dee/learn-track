4fbaa5846866b94808a17c596cf8a132f9043918
Terence Denning <terencedenning@outlook.com>
2022-06-25 22:43:14 +1000

    detail page and added icon

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './options.scss'
 
 const test = <p>Test</p>
 
--- src/pages/tracker.tsx
+++ src/pages/tracker.tsx
@@ -7,9 +7,36 @@ const Tracker = ({ link, setOpenDetail }: any) => {
     console.log({link})
   }, [])
 
+  const displayDays = () => {
+    if ( link.days ) {
+        return link.days.map((day: string) => (
+            <p key={day}>{day}</p>
+        ))
+    }
+  }
+
+  const displayUrls = () => {
+    if ( link.urls ) {
+        return link.urls.map((url: string) => (
+            <p key={url}>{url}</p>
+        ))
+    }
+  }
+
   return (
     <div className="w-full h-full ">
-        <div>Tracker</div>
+        <img
+            className="h-8 w-8"
+            src={`https://s2.googleusercontent.com/s2/favicons?domain_url=${link.urls[0]}`}
+            alt="favicon"
+        />
+        <h1>{link.title}</h1>
+        <div>
+            { displayDays() }
+        </div>
+        <div>
+            { displayUrls() }
+        </div>
         <p className="cursor-pointer" onClick={() => setOpenDetail(false)}>Close</p>
     </div>
     
